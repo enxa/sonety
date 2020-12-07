00c8a11af171b2adce4ab822e2c1fa6c93e71b79
enxa <enxa@mail.com>
2020-12-07 20:01:35 +0100

    easiest parallax

---
+++ src/EasiestParallax.svelte
@@ -0,0 +1,16 @@
+<style>
+  section {
+    width: 100%;
+    min-height: 100vh;
+    background: fixed no-repeat left center / cover;
+    transform: scale(1.1);
+  }
+
+  #section1 { background-image: url('') }
+  #section2 { background-image: url('') }
+  #section3 { background-image: url('') }
+</style>
+
+<section id="section1"></section>
+<section id="section2"></section>
+<section id="section3"></section>
