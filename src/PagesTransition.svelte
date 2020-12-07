fa656b4cb09c45e2b61671fa45f3d679cd3f51de
enxa <enxa@mail.com>
2020-12-07 20:36:59 +0100

    pages transition

---
+++ src/PagesTransition.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { onMount } from 'svelte'
+  import { fly } from 'svelte/transition'
+  import { quintOut } from 'svelte/easing'
+
+  let show = false
+
+  onMount(() => show = true)
+</script>
+
+<style>
+  main {
+    position: fixed;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    width: 101%;
+    height: 100%;
+  }
+</style>
+
+<section>
+  {#if show}
+    <main in:fly="{{ y: 960, duration: 1000, opacity: 0, easing: quintOut }}">
+      <slot></slot>
+    </main>
+  {/if}     
+</section>
