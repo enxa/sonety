a928be97c2f122ea97498f96d96032833e130a0d
enxa <enxa@mail.com>
2020-12-07 19:57:05 +0100

    responsive section

---
+++ src/ResponsiveSection.svelte
@@ -0,0 +1,27 @@
+<style>
+  @media (min-width: 0px)    { section { font-size: 31.25% } }
+  @media (min-width: 320px)  { section { font-size: 37.5% } }
+  @media (min-width: 640px)  { section { font-size: 43.75% } }
+  @media (min-width: 960px)  { section { font-size: 50% } }
+  @media (min-width: 1280px) { section { font-size: 56.25% } }
+  @media (min-width: 1600px) { section { font-size: 62.5% } }
+  @media (min-width: 1920px) { section { font-size: 68.75% } }
+  @media (min-width: 2240px) { section { font-size: 75% } }
+  @media (min-width: 2560px) { section { font-size: 81.25% } }
+  @media (min-width: 2880px) { section { font-size: 87.5% } }
+  @media (min-width: 3200px) { section { font-size: 93.75% } }
+  @media (min-width: 3520px) { section { font-size: 100% } }
+  
+  @media (min-width: 0px)    { section { padding: 0 calc(100vw / 24) } }
+  @media (min-width: 320px)  { section { padding: 0 calc(100vw / 24) } }
+  @media (min-width: 640px)  { section { padding: 0 calc(100vw / 12) } }
+  @media (min-width: 960px)  { section { padding: 0 calc(100vw / 12) } }
+  @media (min-width: 1280px) { section { padding: 0 calc(100vw / 12) } }
+  @media (min-width: 1600px) { section { padding: 0 calc(100vw / 12) } }
+  @media (min-width: 1920px) { section { padding: 0 calc(100vw / 6) } }
+  @media (min-width: 2240px) { section { padding: 0 calc(100vw / 6) } }
+  @media (min-width: 2560px) { section { padding: 0 calc(100vw / 6) } }
+  @media (min-width: 2880px) { section { padding: 0 calc(100vw / 6) } }
+  @media (min-width: 3200px) { section { padding: 0 calc(100vw / 6) } }
+  @media (min-width: 3520px) { section { padding: 0 calc(100vw / 6) } }
+</style>
