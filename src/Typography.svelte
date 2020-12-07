e080e12850182e90c3db073b3f12ff76deeb2604
enxa <enxa@mail.com>
2020-12-07 19:58:13 +0100

    typography

---
+++ src/Typography.svelte
@@ -0,0 +1,31 @@
+<style>
+  :root {
+    --color1: #fdfeff;
+    --color2: #121824;
+    --color3: #1b325e;
+    --color4: #a434ee;
+    --color5: #df62a1;
+    --color6: #ef9e3c;
+
+    --font: 1.5;
+    --font1: calc( var(--font) * 1rem);
+    --font2: calc( var(--font) * var(--font) * 1rem);
+    --font3: calc( var(--font) * var(--font) * var(--font) * 1rem);
+    --font4: calc( var(--font) * var(--font) * var(--font) * var(--font) * 1rem );
+    --font5: calc( var(--font) * var(--font) * var(--font) * var(--font) * var(--font) * 1rem );
+    --font6: calc( var(--font) * var(--font) * var(--font) * var(--font) * var(--font) * var(--font) * 1rem );
+
+    --font-light: 300;
+    --font-regular: 600;
+    --font-bold: 900;
+  }
+
+  p, a, ol, ul, li, span, strong, b, i, em, q, blockquote, copy, input, textarea, button, pre, code { font-size: var(--font1); line-height: var(--font3);}
+        
+  h1 { font-size: var(--font6); line-height: var(--font6); font-weight: var(--font-light); }
+  h2 { font-size: var(--font5); line-height: var(--font5); font-weight: var(--font-light); }
+  h3 { font-size: var(--font4); line-height: var(--font4); font-weight: var(--font-regular); }
+  h4 { font-size: var(--font3); line-height: var(--font3); font-weight: var(--font-regular); }
+  h5 { font-size: var(--font2); line-height: var(--font2); font-weight: var(--font-bold); }
+  h6 { font-size: var(--font1); line-height: var(--font1); font-weight: var(--font-bold); }
+</style>
