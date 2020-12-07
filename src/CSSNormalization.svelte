9958922a806a530341ed076cc82a4fcf53ebf984
enxa <enxa@mail.com>
2020-12-07 19:59:52 +0100

    css normalization

---
+++ src/CSSNormalization.svelte
@@ -0,0 +1,23 @@
+<style>
+  *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+  }
+  html,
+  body,
+  section {
+    font-family: 'Montserrat', 'Avenir', 'Futura', 'Helvetica Neue', sans-serif;
+    font-weight: 300;
+    font-size: 62.5%; /* 1rem = 10px */
+    line-height: 1.5;
+    letter-spacing: 0rem;
+
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+</style>
