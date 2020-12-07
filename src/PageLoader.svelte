de273636ac424b5c83cc252ca6af7bf5e585185b
enxa <enxa@mail.com>
2020-12-07 20:15:28 +0100

    page loader

---
+++ src/PageLoader.svelte
@@ -0,0 +1,5 @@
+<script>
+  window.addEventListener('DOMContentLoaded', () => document.body.innerHTML += loader)
+  window.addEventListener('load', () => document.querySelector('#loader').style.display = "none")     
+  const loader = `<section id="loader"> <svg width="100%" height="100%" viewBox="0 0 100 100"></svg>`
+</script>
