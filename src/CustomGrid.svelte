79503b88e741fe3fb5e15f59f3b2116e8458f924
enxa <enxa@mail.com>
2020-12-07 19:55:53 +0100

    custom grid

---
+++ src/CustomGrid.svelte
@@ -0,0 +1,17 @@
+<style>
+  :root { 
+    --frag: calc(100vw);
+    --frag01: calc(100vw / 1);
+    --frag02: calc(100vw / 2);
+    --frag03: calc(100vw / 3);
+    --frag04: calc(100vw / 4);
+    --frag05: calc(100vw / 5);
+    --frag06: calc(100vw / 6);
+    --frag07: calc(100vw / 7);
+    --frag08: calc(100vw / 8);
+    --frag09: calc(100vw / 9);
+    --frag10: calc(100vw / 10);
+    --frag11: calc(100vw / 11);
+    --frag12: calc(100vw / 12);
+  }
+</style>
