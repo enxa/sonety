78505d1a5f59752c99bdac57161e9ef562e06bcc
enxa <enxa@mail.com>
2020-12-07 20:38:01 +0100

    lerp raf

---
+++ src/LerpRaf.svelte
@@ -0,0 +1,8 @@
+<script>
+  const lerp = (position, targetPosition) => {
+    position.x += (targetPosition.x - position.x) * 0.1
+    position.y += (targetPosition.y - position.y) * 0.1
+    requestAnimationFrame(lerp)
+  }
+  lerp()
+</script>
