a2b87af9600cd8586a199851dab7d58300b8a1e0
enxa <enxa@mail.com>
2020-12-07 20:29:45 +0100

    cursor position detection

---
+++ src/CursorPositionDetection.svelte
@@ -0,0 +1,14 @@
+<script>
+  let mousePosition = { x: 0, y: 0 }
+
+  let handleMousePosition = e => {
+    mousePosition.x = e.x
+    mousePosition.y = e.y
+  }
+</script>
+
+<style>
+  section { width: 100%; height: 100%; }
+</style>
+
+<section on:mousemove="{handleMousePosition}"></section>
