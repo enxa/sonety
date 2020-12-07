d72ca75b0a1019830efd270ea2e3683eca479f98
enxa <enxa@mail.com>
2020-12-07 20:38:58 +0100

    intersection observer

---
+++ src/IntersectionObserver.svelte
@@ -0,0 +1,14 @@
+<script>
+  const toObserve = document.querySelectorAll('.animate');
+
+  observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => 
+      entry.isIntersecting === true || entry.intersectionRatio > 0 ? 
+      entry.target.classList.add('visible') : 
+      entry.target.classList.remove('visible'))
+  })
+
+  toObserve.forEach(image => {
+      observer.observe(image)
+  })
+</script>
